fd339030ff5686ea41ad5ced1a51683f3ffe4d02
Ítalo Rufca <italo.rufca@gmail.com>
2026-04-23 14:57:27 -0300

    Initial Commit

---
+++ Biriapp/BiriappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiriappApp.swift
+//  Biriapp
+//
+//  Created by Aretê Colors on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BiriappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
